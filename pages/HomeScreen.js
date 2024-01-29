aaba46e552caf73d79b8a5c8e49ab46ef2bfe607
LileoC <LileoC@proton.me>
2024-01-29 16:39:00 +0100

    edit home screen

--- pages/HomeScreen.js
+++ pages/HomeScreen.js
@@ -1,9 +1,13 @@
-import { Text, View } from "react-native";
+import { Button, View } from "react-native";
+
+// useEffect to dispatch a thunk that fetches if the game is active or not.
+// if active, join a game. else start a game
 
 export default function HomeScreen() {
   return (
     <View>
-      <Text>Home</Text>
+      <Button>Start a game</Button>
+      <Button>Join a game</Button>
     </View>
   );
 }
