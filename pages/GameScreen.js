1251dcee2a3ed966e99f6c7f362f88a1f17122c6
AldrinKP <aldrinpastores@gmail.com>
2024-01-29 16:50:15 +0100

    Removed button and isPlaying state

--- pages/GameScreen.js
+++ pages/GameScreen.js
@@ -3,15 +3,13 @@ import { Text, View, StyleSheet, Button } from 'react-native';
 import { CountdownCircleTimer } from 'react-native-countdown-circle-timer';
 
 export default function GameScreen() {
-	const [isPlaying, setIsPlaying] = React.useState(true);
-
 	return (
 		<View style={styles.container}>
 			<Text style={{ fontSize: 36, marginBottom: 40 }}>
 				Game starts in....{' '}
 			</Text>
 			<CountdownCircleTimer
-				isPlaying={isPlaying}
+				isPlaying={true}
 				duration={10}
 				colors={['#004777', '#F7B801', '#A30000', '#A30000']}
 				colorsTime={[10, 6, 3, 0]}
@@ -22,10 +20,6 @@ export default function GameScreen() {
 					<Text style={{ color, fontSize: 40 }}>{remainingTime}</Text>
 				)}
 			</CountdownCircleTimer>
-			<Button
-				title="Toggle Playing"
-				onPress={() => setIsPlaying((prev) => !prev)}
-			/>
 		</View>
 	);
 }
