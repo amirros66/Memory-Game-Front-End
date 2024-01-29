59f3bf3d842884d72ea4beabd2234c6408b740fe
AldrinKP <aldrinpastores@gmail.com>
2024-01-29 16:45:23 +0100

    Created game screen with countdown timer

--- pages/GameScreen.js
+++ pages/GameScreen.js
@@ -1,9 +1,41 @@
-import { Text, View } from "react-native";
+import * as React from 'react';
+import { Text, View, StyleSheet, Button } from 'react-native';
+import { CountdownCircleTimer } from 'react-native-countdown-circle-timer';
 
 export default function GameScreen() {
-  return (
-    <View>
-      <Text>Game Screen</Text>
-    </View>
-  );
+	const [isPlaying, setIsPlaying] = React.useState(true);
+
+	return (
+		<View style={styles.container}>
+			<Text style={{ fontSize: 36, marginBottom: '30px' }}>
+				Game starts in....{' '}
+			</Text>
+			<CountdownCircleTimer
+				isPlaying={isPlaying}
+				duration={10}
+				colors={['#004777', '#F7B801', '#A30000', '#A30000']}
+				colorsTime={[10, 6, 3, 0]}
+				onComplete={() => ({ shouldRepeat: true, delay: 2 })}
+				updateInterval={1}
+			>
+				{({ remainingTime, color }) => (
+					<Text style={{ color, fontSize: 40 }}>{remainingTime}</Text>
+				)}
+			</CountdownCircleTimer>
+			<Button
+				title="Toggle Playing"
+				onPress={() => setIsPlaying((prev) => !prev)}
+			/>
+		</View>
+	);
 }
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: '#ecf0f1',
+		padding: 8,
+	},
+});
