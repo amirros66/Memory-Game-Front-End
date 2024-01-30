26d482514a49ab3cb50d6637b82346102040f0a0
amirros66 <143261344+amirros66@users.noreply.github.com>
2024-01-30 11:51:17 +0100

    Add text to Lobby screen page.

--- pages/LobbyScreen.js
+++ pages/LobbyScreen.js
@@ -1,9 +1,37 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
+// import { useSelector } from "react-redux";
+// import { useEffect } from "react";
+// import { selectIsGameReady } from "../store/selectors";
 
-export default function LobbyScreen() {
+export default function LobbyScreen({ navigation }) {
+  // const isGameReady = useSelector(selectIsGameReady);
+
+  // useEffect(() => {
+  //   if (isGameReady.length === 3) {
+  //     navigation.navigate("Game");
+  //   }
+  // }, [isGameReady, navigation]);
+
   return (
-    <View>
-      <Text>Lobby</Text>
+    <View style={styles.container}>
+      {/* <Text style={{ fontSize: 30, marginTop: 40, marginBottom: 40 }}>
+        {isGameReady.length === 3
+          ? "Starting the game..."
+          : `Waiting for all players to start the game... (${isGameReady.length}/3)`}
+      </Text> */}
+      <Text>
+        Waiting for all players to join the lobby before the game starts...
+      </Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+    padding: 8,
+  },
+});
