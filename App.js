fdb2dd77f4b9bc86dc93c440414abbdcca5dc0fa
AldrinKP <aldrinpastores@gmail.com>
2024-01-30 09:50:36 +0100

    Added display and input sequence screens to nav stack

--- App.js
+++ App.js
@@ -1,32 +1,29 @@
-import * as React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import HomeScreen from "./pages/HomeScreen";
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './pages/HomeScreen';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import GameScreen from './pages/GameScreen';
+import DisplaySequenceScreen from './pages/DisplaySequenceScreen';
+import InputSequenceScreen from './pages/InputSequenceScreen';
 const Stack = createStackNavigator();
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Game" component={GameScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+	return (
+		<NavigationContainer>
+			<Stack.Navigator>
+				<Stack.Screen name="Home" component={HomeScreen} />
+				<Stack.Screen name="Game" component={GameScreen} />
+				<Stack.Screen
+					name="DisplaySequence"
+					component={DisplaySequenceScreen}
+				/>
+				<Stack.Screen
+					name="InputSequence"
+					component={InputSequenceScreen}
+				/>
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-});
-
-
-
-
