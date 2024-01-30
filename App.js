35f847259575005408e80a3a55cb0ab3693b74f4
amirros66 <143261344+amirros66@users.noreply.github.com>
2024-01-30 10:45:09 +0100

    Add LobbyScreen to app.js navigation

--- App.js
+++ App.js
@@ -1,29 +1,28 @@
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import HomeScreen from './pages/HomeScreen';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import GameScreen from './pages/GameScreen';
-import DisplaySequenceScreen from './pages/DisplaySequenceScreen';
-import InputSequenceScreen from './pages/InputSequenceScreen';
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import HomeScreen from "./pages/HomeScreen";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import GameScreen from "./pages/GameScreen";
+import DisplaySequenceScreen from "./pages/DisplaySequenceScreen";
+import InputSequenceScreen from "./pages/InputSequenceScreen";
+import LobbyScreen from "./pages/LobbyScreen";
 const Stack = createStackNavigator();
 
 export default function App() {
-	return (
-		<NavigationContainer>
-			<Stack.Navigator>
-				<Stack.Screen name="Home" component={HomeScreen} />
-				<Stack.Screen name="Game" component={GameScreen} />
-				<Stack.Screen
-					name="DisplaySequence"
-					component={DisplaySequenceScreen}
-				/>
-				<Stack.Screen
-					name="InputSequence"
-					component={InputSequenceScreen}
-				/>
-			</Stack.Navigator>
-		</NavigationContainer>
-	);
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Lobby" component={LobbyScreen} />
+        <Stack.Screen name="Game" component={GameScreen} />
+        <Stack.Screen
+          name="DisplaySequence"
+          component={DisplaySequenceScreen}
+        />
+        <Stack.Screen name="InputSequence" component={InputSequenceScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
