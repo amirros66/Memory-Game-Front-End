d35204a7c5092334e0a1d8e8f0d0e0d8e09b67c7
LileoC <LileoC@proton.me>
2024-01-30 12:00:01 +0100

    add animations

--- pages/GameOverScreen.js
+++ pages/GameOverScreen.js
@@ -1,24 +1,67 @@
 import { Text, View, StyleSheet } from "react-native";
+import * as Animatable from "react-native-animatable";
+import React, { useRef, useEffect } from "react";
 
 export default function GameOverScreen() {
   const firstPlace = "Player 1";
   const secondPlace = "Player 2";
   const thirdPlace = "Player 3";
 
+  const animateText1Ref = useRef(null);
+  const animateText2Ref = useRef(null);
+  const animateText3Ref = useRef(null);
+
+  useEffect(() => {
+    if (animateText1Ref.current) {
+      animateText1Ref.current.bounceIn();
+      setTimeout(() => {
+        if (animateText2Ref.current) {
+          animateText2Ref.current.bounceIn();
+          setTimeout(() => {
+            if (animateText3Ref.current) {
+              animateText3Ref.current.bounceIn();
+            }
+          }, 2000);
+        }
+      }, 4000);
+    }
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text style={{ fontSize: 40, fontWeight: "bold", marginBottom: 15 }}>
         🥇1st Place🥇
       </Text>
-      <Text style={{ fontSize: 30, marginBottom: 15 }}>{firstPlace}</Text>
+      <Animatable.Text
+        ref={animateText1Ref}
+        animation="bounceIn"
+        iterationCount={4}
+        style={{ fontSize: 30, marginBottom: 15 }}
+      >
+        {firstPlace}
+      </Animatable.Text>
       <Text style={{ fontSize: 37, fontWeight: "bold", marginBottom: 15 }}>
         🥈2nd Place🥈
       </Text>
-      <Text style={{ fontSize: 30, marginBottom: 15 }}>{secondPlace}</Text>
+      <Animatable.Text
+        ref={animateText2Ref}
+        animation="bounceIn"
+        iterationCount={2}
+        style={{ fontSize: 30, marginBottom: 15 }}
+      >
+        {secondPlace}
+      </Animatable.Text>
       <Text style={{ fontSize: 35, fontWeight: "bold", marginBottom: 15 }}>
         🥉3rd Place🥉
       </Text>
-      <Text style={{ fontSize: 30, marginBottom: 15 }}>{thirdPlace}</Text>
+      <Animatable.Text
+        ref={animateText3Ref}
+        animation="bounceIn"
+        iterationCount={2}
+        style={{ fontSize: 30, marginBottom: 15 }}
+      >
+        {thirdPlace}
+      </Animatable.Text>
     </View>
   );
 }
