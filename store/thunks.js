f6c476d1bb0c99c0d54c0af5b93088af5d682282
AldrinKP <aldrinpastores@gmail.com>
2024-02-01 14:54:07 +0100

    Created thunk to retrieve active game from back end

--- store/thunks.js
+++ store/thunks.js
@@ -1,3 +1,14 @@
 import axios from 'axios';
+import { setGameID } from './slice';
 
-const API_BASE_URL = 'http://127.0.0.1:8000';
+const API_BASE_URL = 'http://192.168.68.59:8000';
+
+export const fetchActiveGame = () => async (dispatch) => {
+	try {
+		const response = await axios.get(`${API_BASE_URL}/game/active`);
+		const game = response.data;
+		dispatch(setGameID(game.id));
+	} catch (error) {
+		dispatch(setGameID(null));
+	}
+};
