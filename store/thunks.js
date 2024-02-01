bc85c8ff0cfb170ac9eb4019ce4e4cefc1fab90d
LileoC <LileoC@proton.me>
2024-02-01 15:16:16 +0100

    edit thunk

--- store/thunks.js
+++ store/thunks.js
@@ -2,14 +2,17 @@ import axios from "axios";
 import { API_BASE_URL } from "../.env";
 import { setDisplaySequences } from "./slice";
 
+console.log("API_BASE_URL:", API_BASE_URL);
+
 export const getDisplaySequencesThunk = (game_id) => {
   return async (dispatch) => {
     try {
       const response = await axios.get(
         `${API_BASE_URL}/display-sequences?game_id=${game_id}`
       );
-      dispatch(setDisplaySequences(response.data));
-      console.log("Response data:", response.data);
+      const display_sequences = response.data;
+      dispatch(setDisplaySequences(display_sequences));
+      console.log("Display Sequences:", display_sequences);
     } catch (error) {
       console.log("Error:", error);
     }
