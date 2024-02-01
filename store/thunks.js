3cac6e7483e1ba1fa15875c2e4fff6eebdb603ac
AldrinKP <aldrinpastores@gmail.com>
2024-02-01 15:24:36 +0100

    Created thunk to call post /game endpoint

--- store/thunks.js
+++ store/thunks.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-import { setGameID } from './slice';
+import { setGameID, setGame } from './slice';
+import store from './index';
 
 const API_BASE_URL = 'http://192.168.68.59:8000';
 
@@ -8,7 +9,19 @@ export const fetchActiveGame = () => async (dispatch) => {
 		const response = await axios.get(`${API_BASE_URL}/game/active`);
 		const game = response.data;
 		dispatch(setGameID(game.id));
-	} catch (error) {
+	} catch {
 		dispatch(setGameID(null));
+	}
+};
+
+export const createGame = () => async (dispatch) => {
+	try {
+		const response = await axios.post(`${API_BASE_URL}/game`);
+		const game = response.data;
+		dispatch(setGame(game));
+		console.log(game);
+		console.log(store.getState().game);
+	} catch (error) {
+		console.log(error);
 	}
 };
