0ab5c0279650e8867dcfb91ee42b8a59fb341919
LileoC <LileoC@proton.me>
2024-02-01 11:43:14 +0100

    refactor to wrap app with store

--- App.js
+++ App.js
@@ -8,49 +8,53 @@ import DisplaySequenceScreen from "./pages/DisplaySequenceScreen";
 import InputSequenceScreen from "./pages/InputSequenceScreen";
 import ResultsScreen from "./pages/ResultsScreen";
 import GameOverScreen from "./pages/GameOverScreen";
+import { Provider } from "react-redux";
+import store from "./store";
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Lobby"
-          component={LobbyScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Game"
-          component={GameScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="DisplaySequence"
-          component={DisplaySequenceScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="InputSequence"
-          component={InputSequenceScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Results"
-          component={ResultsScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="GameOver"
-          component={GameOverScreen}
-          options={{ headerShown: false }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Lobby"
+            component={LobbyScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Game"
+            component={GameScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="DisplaySequence"
+            component={DisplaySequenceScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="InputSequence"
+            component={InputSequenceScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Results"
+            component={ResultsScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="GameOver"
+            component={GameOverScreen}
+            options={{ headerShown: false }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 }
