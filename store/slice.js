673c75b971d88e4e45865ea63bdb650b77e989fb
AldrinKP <aldrinpastores@gmail.com>
2024-02-01 14:53:52 +0100

    Created reducer to set game id from api call

--- store/slice.js
+++ store/slice.js
@@ -1,16 +1,21 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 
 initialState = {
-  game_id: null,
-  users: [],
-  display_sequences: [],
-  round: 1,
+	game_id: null,
+	users: [],
+	display_sequences: [],
+	round: 1,
 };
 
 const gameSlice = createSlice({
-  name: "game",
-  initialState,
-  reducers: {},
+	name: 'game',
+	initialState,
+	reducers: {
+		setGameID: (state, action) => {
+			state.game_id = action.payload;
+		},
+	},
 });
 
+export const { setGameID } = gameSlice.actions;
 export default gameSlice.reducer;
