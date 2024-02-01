62deee12df1b00bfaf6b99a537cf9cd329682f6a
amirros66 <143261344+amirros66@users.noreply.github.com>
2024-02-01 18:45:20 +0100

    use selector to display sequence in display sequence page, after game screen page navigates to it.

--- pages/DisplaySequenceScreen.js
+++ pages/DisplaySequenceScreen.js
@@ -1,49 +1,56 @@
-import { Text, View, StyleSheet } from 'react-native';
-import { CountdownCircleTimer } from 'react-native-countdown-circle-timer';
+import { Text, View, StyleSheet } from "react-native";
+import { CountdownCircleTimer } from "react-native-countdown-circle-timer";
+import { useSelector } from "react-redux";
+import { selectDisplaySequences } from "../store/selectors";
 
 export default function DisplaySequenceScreen({ navigation }) {
-	// Replace with dynamic values
-	const round = 1;
-	const sequence = '⬅️ ⬆️ ⬇️ ➡️';
+  const displaySequence = useSelector(selectDisplaySequences);
+  console.log("Display Sequence:", displaySequence); //returns empty array
 
-	setTimeout(() => {
-		navigation.navigate('InputSequence');
-	}, 10000);
+  const sequenceToDisplay = displaySequence[0]?.value || "Sequence not found";
 
-	return (
-		<View style={styles.container}>
-			<Text
-				style={{ fontSize: 40, fontWeight: 'bold', marginBottom: 15 }}
-			>
-				Round {round}
-			</Text>
-			<Text style={{ fontSize: 25, marginBottom: 40 }}>
-				Remember the sequence!
-			</Text>
-			<Text style={{ fontSize: 40, marginBottom: 200 }}>{sequence}</Text>
-			<CountdownCircleTimer
-				isPlaying={true}
-				duration={10}
-				colors={['#004777', '#F7B801', '#A30000', '#A30000']}
-				colorsTime={[10, 6, 3, 0]}
-				onComplete={() => ({ shouldRepeat: true, delay: 2 })}
-				updateInterval={1}
-				size={80}
-			>
-				{({ remainingTime, color }) => (
-					<Text style={{ color, fontSize: 40 }}>{remainingTime}</Text>
-				)}
-			</CountdownCircleTimer>
-		</View>
-	);
+  // Replace with dynamic values
+  const round = 1;
+  // const sequence = '⬅️ ⬆️ ⬇️ ➡️';
+
+  setTimeout(() => {
+    navigation.navigate("InputSequence");
+  }, 10000);
+
+  return (
+    <View style={styles.container}>
+      <Text style={{ fontSize: 40, fontWeight: "bold", marginBottom: 15 }}>
+        Round {round}
+      </Text>
+      <Text style={{ fontSize: 25, marginBottom: 40 }}>
+        Remember the sequence!
+      </Text>
+      <Text style={{ fontSize: 40, marginBottom: 200 }}>
+        {sequenceToDisplay}
+      </Text>
+      <CountdownCircleTimer
+        isPlaying={true}
+        duration={10}
+        colors={["#004777", "#F7B801", "#A30000", "#A30000"]}
+        colorsTime={[10, 6, 3, 0]}
+        onComplete={() => ({ shouldRepeat: true, delay: 2 })}
+        updateInterval={1}
+        size={80}
+      >
+        {({ remainingTime, color }) => (
+          <Text style={{ color, fontSize: 40 }}>{remainingTime}</Text>
+        )}
+      </CountdownCircleTimer>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		justifyContent: 'center',
-		alignItems: 'center',
-		backgroundColor: '#ecf0f1',
-		padding: 8,
-	},
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#ecf0f1",
+    padding: 8,
+  },
 });
