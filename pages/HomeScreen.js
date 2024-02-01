af975acf84a5deae9739da4348135b3777829192
AldrinKP <aldrinpastores@gmail.com>
2024-02-01 14:54:31 +0100

    Added functionality to get active game and display buttons

--- pages/HomeScreen.js
+++ pages/HomeScreen.js
@@ -1,10 +1,21 @@
 import { Button, View } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchActiveGame } from '../store/thunks';
+import { selectActiveGameID } from '../store/selectors';
 
 // useEffect to dispatch a thunk that fetches if the game is active or not.
 // if active, join a game. else start a game
 // when either button is pressed, make a post request to add a new user/player
 
 export default function HomeScreen({ navigation }) {
+	const dispatch = useDispatch();
+	const gameID = useSelector(selectActiveGameID);
+
+	useEffect(() => {
+		dispatch(fetchActiveGame());
+	});
+
 	function handleStartGame() {
 		//dispatch thunk to start game
 		navigation.navigate('Lobby');
@@ -19,9 +30,11 @@ export default function HomeScreen({ navigation }) {
 		<View
 			style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
 		>
-			<Button title="Start a game" onPress={handleStartGame} />
-			<View style={{ marginVertical: 10 }} />
-			<Button title="Join a game" onPress={handleJoinGame} />
+			{gameID ? (
+				<Button title="Join a game" onPress={handleJoinGame} />
+			) : (
+				<Button title="Start a game" onPress={handleStartGame} />
+			)}
 
 			{/* Testing */}
 			<Button
