e6bc48a91006d9aaf7fed9acdb5714b382dcec94
AldrinKP <aldrinpastores@gmail.com>
2024-02-02 12:02:33 +0100

    Created a new set round action

--- store/slice.js
+++ store/slice.js
@@ -1,58 +1,62 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 
 initialState = {
-  game_id: null,
-  user_id: null,
-  users: [],
-  display_sequences: [],
-  round: 1,
-  timeout: null,
-  results: [],
-  loading: true,
-  finalResults: [],
+	game_id: null,
+	user_id: null,
+	users: [],
+	display_sequences: [],
+	round: 1,
+	timeout: null,
+	results: [],
+	loading: true,
+	finalResults: [],
 };
 
 const gameSlice = createSlice({
-  name: "game",
-  initialState,
-  reducers: {
-    setDisplaySequences(state, action) {
-      state.display_sequences = action.payload;
-    },
-    setGameID: (state, action) => {
-      state.game_id = action.payload;
-    },
-    setGame: (state, action) => {
-      state.game_id = action.payload.game_id;
-      state.user_id = action.payload.user_id;
-    },
-    setUserID: (state, action) => {
-      state.user_id = action.payload.id;
-    },
-    setUsers: (state, action) => {
-      state.users = action.payload;
-    },
-    setResults: (state, action) => {
-      state.results = action.payload;
-    },
-    setLoading: (state, action) => {
-      state.loading = action.payload;
-    },
-    setFinalResults: (state, action) => {
-      state.finalResults = action.payload;
-    },
-  },
+	name: 'game',
+	initialState,
+	reducers: {
+		setDisplaySequences(state, action) {
+			state.display_sequences = action.payload;
+		},
+		setGameID: (state, action) => {
+			state.game_id = action.payload;
+		},
+		setGame: (state, action) => {
+			state.game_id = action.payload.game_id;
+			state.user_id = action.payload.user_id;
+		},
+		setUserID: (state, action) => {
+			state.user_id = action.payload.id;
+		},
+		setUsers: (state, action) => {
+			state.users = action.payload;
+		},
+		setResults: (state, action) => {
+			state.results = action.payload;
+		},
+		setLoading: (state, action) => {
+			state.loading = action.payload;
+		},
+		setFinalResults: (state, action) => {
+			state.finalResults = action.payload;
+		},
+		setRound: (state, action) => {
+			state.round = action.payload;
+		},
+	},
 });
 
 export const {
-  setGame,
-  setGameID,
-  setUserID,
-  setDisplaySequences,
-  setUsers,
-  setResults,
-  setLoading,
-  setFinalResults,
+	setGame,
+	setGameID,
+	setUserID,
+	setDisplaySequences,
+	setUsers,
+	setResults,
+	setLoading,
+	setFinalResults,
+	setRound,
 } = gameSlice.actions;
 
 export default gameSlice.reducer;
